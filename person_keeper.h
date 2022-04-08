31335939600017ee5f99049b49738a67274745d0
Vasiliy <manv_v@mail.ru>
2022-04-08 22:44:54 +0300

    инициализация класса Person_keeper

--- person_keeper.h
+++ person_keeper.h
@@ -1,7 +1,25 @@
 #ifndef PERSON_KEEPER_H
 #define PERSON_KEEPER_H
+#include <QString>
+#include "stack.h"
+#include "person.h"
 
+class PersonKeeper
+{
+private:
+    PersonKeeper(){}
+    PersonKeeper(const PersonKeeper &);
+    ~PersonKeeper(){}
+    PersonKeeper &operator=(const PersonKeeper &);
+    Stack<Person> s;
+public:
+    static PersonKeeper &Instance();
+    void ReadPersons(QString path);
+    void WritePersons(QString path) const;
+    int Size();
+    void Clear();
 
+};
 
 
 
