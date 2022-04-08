fd30b71f26ab7dd3558f1db0f2df9541e78f3284
Vasiliy <manv_v@mail.ru>
2022-04-08 15:56:04 +0300

    Инициализация методов класса person и добавление комментариев

--- person.h
+++ person.h
@@ -1,32 +1,46 @@
 #ifndef PERSON_H
 #define PERSON_H
 #include <QString>
+#include <QRegularExpression>
+
 
 class Person
 {
 private:
-    QString l_name;
-    QString f_name;
-    QString p_name;
+    QString l_name;//фамилия
+    QString f_name;//имя
+    QString p_name;//отчество
 public:
-    Person(){}
-    Person(QString l_name, QString f_name, QString patronymic): l_name(l_name), f_name(f_name), p_name(p_name)
-    { }
+    Person(){}// конструктор по умолчанию
+    Person(QString l_name, QString f_name, QString p_name): l_name(l_name), f_name(f_name), p_name(p_name){ }//конструктор с аргументами
     Person(QString full_name);
     void Set_l_name(const QString &l_name) {
         this->l_name = l_name;
-    }
+    } //setter фамилии
     void Set_f_name(const QString &f_name) {
         this->f_name = f_name;
-    }
+    }//setter имя
     void Set_p_name(const QString &p_name) {
         this->p_name = p_name;
-    }
-    const QString &Get_l_name() const { return l_name; }
-    const QString &Get_f_name() const { return f_name; }
-    const QString &Get_p_name() const { return p_name; }
+    }//setter отчества
+    const QString &Get_l_name() const { return l_name; }//getter фамилии
+    const QString &Get_f_name() const { return f_name; }//getter имени
+    const QString &Get_p_name() const { return p_name; }//getter отвество
 
 };
+
+Person::Person(QString f_name)//метод разбиения строки на подстроки:фамилия , имя , отчество.Личность может не имять отчество.
+{
+    QStringList str = f_name.split(QRegularExpression("\\s+"), Qt::SkipEmptyParts);//разбиение
+    if (str.size() != 2 && str.size() != 3)//если размер строки не равен 2 , 3 ,то выводим сообщение в ошибке
+    { throw "Error size strok";}
+    l_name = str[0];//присваиваем фамилию
+    f_name = str[1];//присваиваем имя
+    if (str.size() == 3)
+    {
+        p_name = str[2];//присваиваем отчество
+    }
+}
 
 #endif // PERSON_H
 
